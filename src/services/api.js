496de74cbadbf1fd408c0b4c0b3f3bcca48ef303
KevinKur <kevinmlg2004@gmail.com>
2026-07-09 14:58:46 +0700

    Back to before

--- src/services/api.js
+++ src/services/api.js
@@ -1,51 +1,14 @@
-// const BASE_URL = "https://fakestoreapi.com";
-
-// export async function getProducts() {
-//   const response = await fetch(`${BASE_URL}/products?limit=5`, {
-//     cache: "no-store",
-//   });
-
-//   if (!response.ok) {
-//     console.error("STATUS:", response.status);
-//     console.error(await response.text());
-
-//     throw new Error("Failed to fetch products");
-//   }
-
-//   return response.json();
-// }
-
-// export async function getProductById(id) {
-//   const response = await fetch(`${BASE_URL}/products/${id}`, {
-//     cache: "no-store",
-//   });
-
-//   if (response.status === 404) {
-//     return null;
-//   }
-
-//   if (!response.ok) {
-//     console.error("STATUS:", response.status);
-//     console.error(await response.text());
-
-//     throw new Error("Failed to fetch product");
-//   }
-
-//   return response.json();
-// }
-
 const BASE_URL = "https://fakestoreapi.com";
 
 export async function getProducts() {
   const response = await fetch(`${BASE_URL}/products?limit=5`, {
     cache: "no-store",
-    headers: {
-      Accept: "application/json",
-      "User-Agent": "Mozilla/5.0",
-    },
   });
 
   if (!response.ok) {
+    console.error("STATUS:", response.status);
+    console.error(await response.text());
+
     throw new Error("Failed to fetch products");
   }
 
@@ -55,10 +18,6 @@ export async function getProducts() {
 export async function getProductById(id) {
   const response = await fetch(`${BASE_URL}/products/${id}`, {
     cache: "no-store",
-    headers: {
-      Accept: "application/json",
-      "User-Agent": "Mozilla/5.0",
-    },
   });
 
   if (response.status === 404) {
@@ -66,6 +25,9 @@ export async function getProductById(id) {
   }
 
   if (!response.ok) {
+    console.error("STATUS:", response.status);
+    console.error(await response.text());
+
     throw new Error("Failed to fetch product");
   }
 
