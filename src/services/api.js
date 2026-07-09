85460caa1609716adfbf368b174a98320770097f
KevinKur <kevinmlg2004@gmail.com>
2026-07-09 14:32:09 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,14 @@
 const BASE_URL = "https://fakestoreapi.com";
 
 export async function getProducts() {
-  const response = await fetch(`${BASE_URL}/products?limit=5`);
+  const response = await fetch(`${BASE_URL}/products?limit=5`, {
+    cache: "no-store",
+  });
 
   if (!response.ok) {
+    console.error("STATUS:", response.status);
+    console.error(await response.text());
+
     throw new Error("Failed to fetch products");
   }
 
@@ -11,13 +16,18 @@ export async function getProducts() {
 }
 
 export async function getProductById(id) {
-  const response = await fetch(`${BASE_URL}/products/${id}`);
+  const response = await fetch(`${BASE_URL}/products/${id}`, {
+    cache: "no-store",
+  });
 
   if (response.status === 404) {
     return null;
   }
 
   if (!response.ok) {
+    console.error("STATUS:", response.status);
+    console.error(await response.text());
+
     throw new Error("Failed to fetch product");
   }
 
